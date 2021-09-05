88777c414dc56f8999c77001bacbb3a2f277ddda
kozistr <kozistr@gmail.com>
2021-09-05 17:18:03 +0900

    update: dark mode color palatte

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,22 +1,12 @@
 export default {
   initialColorModeName: 'light',
-  fonts: {
-    body: 'system-ui, sans-serif',
-    heading: '"Avenir Next", sans-serif',
-    monospace: 'Menlo, monospace',
-  },
   colors: {
     text: '#24292e',
     background: '#fff',
-    primary: '#07c',
-    accent: '#333',
-    muted: '#a8a8a8',
     modes: {
       dark: {
-        text: '#fff',
-        background: '#222222',
-        secondary: '#fff',
-        muted: '#f6f6f6',
+        text: '#e4e4e4',
+        background: '#141414',
       },
       tomato: {
         text: '#565656',
