c22322ea03877a0a3458040fdcf40d96dc8bc53c
kozistr <kozistr@gmail.com>
2021-09-05 18:45:44 +0900

    update: color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,10 @@ import { graphql, Link } from 'gatsby';
 import { FontAwesomeIcon as Fa } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
+import './styles/index.scss';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import Bio from '../components/Bio';
-import './styles/index.scss';
 import PostList from '../components/PostList';
 
 interface IndexPageProps {
