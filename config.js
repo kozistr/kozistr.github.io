076204c2607f11f1a1d36f13a8781e3c753ff880
kozistr <kozistr@gmail.com>
2022-11-20 20:21:40 +0900

    update: gitcus

--- config.js
+++ config.js
@@ -29,6 +29,13 @@ module.exports = {
   disqusShortname: 'zer0days-blog',     // comments (Disqus sort-name)
   enableSocialShare: true,              // Social share icon activation (Type of Value: Boolean. Not String)
 
+  /** Comment */
+  repo: 'kozistr/kozistr.github.io',
+  repoId: 'MDEwOlJlcG9zaXRvcnkxMjE2MTUxNzk=',
+  category: 'Comment',
+  categoryId: 'DIC_kwDOBz-zS84CSqPm',
+  mapping: 'url',
+
   /** Optional */
   googleAnalytics: 'UA-116366668-1',    // Google Analytics TrackingID. ex.'UA-123456789-0'
   googleSearchConsole: '',              // content value in HTML tag of google search console ownership verification. ex. 'yVb-5RupSxz0n597I3F2TyflhbyZqUs8rHyLmrzqJB4'
