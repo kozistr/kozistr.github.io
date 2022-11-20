107e40caffe7dbfc3c57e12e92641522dcfc334a
kozistr <kozistr@gmail.com>
2022-11-20 21:17:09 +0900

    fix: missing div

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -5,21 +5,23 @@ import config from '../../../config'
 
 const Comment = () => {
   return (
-    <Giscus
-      id="comments"
-      repo="kozistr/kozistr.github.io"
-      repoId={config.repoId}
-      category={config.category}
-      categoryId={config.categoryId}
-      mapping="url"
-      term="Welcome to giscus!"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="bottom"
-      theme="preferred_color_scheme"
-      lang="en"
-      loading="lazy"
-    />
+    <div className="comments">
+      <Giscus
+        id="comments"
+        repo="kozistr/kozistr.github.io"
+        repoId={config.repoId}
+        category={config.category}
+        categoryId={config.categoryId}
+        mapping="url"
+        term="Welcome to giscus!"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme="preferred_color_scheme"
+        lang="en"
+        loading="lazy"
+      />
+    </div>
   )
 }
 
