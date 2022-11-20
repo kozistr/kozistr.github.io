5472505b5f4dc85df6cb7b74756b9ad485b8e55a
kozistr <kozistr@gmail.com>
2022-11-20 22:34:24 +0900

    update: remove discus tag

--- config.js
+++ config.js
@@ -26,7 +26,6 @@ module.exports = {
 
   /** Post */
   enablePostOfContents: true,           // TableOfContents activation (Type of Value: Boolean. Not String)
-  disqusShortname: 'zer0days-blog',     // comments (Disqus sort-name)
   enableSocialShare: true,              // Social share icon activation (Type of Value: Boolean. Not String)
 
   /** Comment */
