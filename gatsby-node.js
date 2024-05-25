6dd482e9e5d3bbaf003002f6e3726e3347d38e9b
kozistr <kozistr@gmail.com>
2024-05-25 20:49:31 +0900

    update: gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -89,24 +89,16 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   )
 }
 
-exports.onCreateNode = async ({ node, actions, getNode }) => {
+exports.onCreateNode = ({ node, actions, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
 
     const rewriteSlug = slug => {
-      // 폴더 경로에 따라 url에 표시되는 것을 폴더 경로를 제거하고 파일명으로만 url을 지정되도록 하기 위함
       if (slug.match(/\//g).length > 2) {
-        let tempStr = slug.split('/')
-        slug = `/${tempStr[tempStr.length - 2]}/`
+        return `/${slug.split('/').slice(-2, -1)[0]}/`
       }
 
-      // jekyll 기준으로 파일명에 날짜를 포함시키던 것을 url에서 제거하기 위함
-      const dayRegExp = /\/(18|19|20|21)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])-/
-      if (slug.match(dayRegExp)) {
-        slug = '/' + slug.replace(dayRegExp, '')
-      }
-
-      return slug
+      return slug.replace(/\/(18|19|20|21)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])-/, '/')
     }
 
     const rewriteNode = node => {
@@ -119,22 +111,16 @@ exports.onCreateNode = async ({ node, actions, getNode }) => {
         `)
       }
 
-      // 마크다운 파일 내 keywords 필드가 비어있을 시 오류가 나지 않도록 하기 위함
       if (!node.frontmatter.keywords) {
         node.frontmatter.keywords = [config.title, config.author]
       }
 
-      // 마크다운 파일 내 태그 필드가 비어있을 시 오류가 나지 않도록 하기 위함
-      if (!node.frontmatter.tags || node.frontmatter.tags === '') {
+      if (!node.frontmatter.tags) {
         node.frontmatter.tags = ['undefined']
-      }
-
-      // 태그 필드가 배열이 아닌 문자열 하나일때 배열로 덮음
-      else if (typeof node.frontmatter.tags === 'string') {
+      } else if (typeof node.frontmatter.tags === 'string') {
         node.frontmatter.tags = [node.frontmatter.tags]
       }
 
-      // markdown 내 date의 timezone 제거
       if (node.frontmatter.date.includes('+')) {
         node.frontmatter.date = new Date(node.frontmatter.date.split('+')[0])
       } else {
