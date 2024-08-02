4d77635a8edb9346b2c224c892527daf697d8651
kozistr <kozistr@gmail.com>
2024-08-02 18:16:37 +0900

    update: remove nanum gothic

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -3,8 +3,8 @@ import Typography from 'typography'
 const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: 1.666,
-  headerFontFamily: ['Apple SD Gothic Neo', 'Nanum Gothic'],
-  bodyFontFamily: ['Apple SD Gothic Neo', 'Nanum Gothic'],
+  headerFontFamily: ['Apple SD Gothic Neo'],
+  bodyFontFamily: ['Apple SD Gothic Neo'],
 })
 
 export default typography
